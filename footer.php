d07b8272b5430a85f124e5b8a17f3db275366e7d
Daniel van der Velden <danielvandervelden@Daniels-MacBook-Pro.local>
2018-10-06 16:59:16 +0200

    Couple fixes. Start of search.

--- footer.php
+++ footer.php
@@ -23,20 +23,18 @@
                      </div>
                   </div>
                </footer>
-			   <div class="search-overlay no-display">
-				<div class="search-overlay__top">
-				  <div class="search-container-balk">
-					<i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
-					<input class="search-term" placeholder="Waar ben je naar op zoek?" id="search-term" name="searchfield" type="text">
-				  <span id="search-close"><i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i></span>
-				  </div>
-					  <div class="search-container">
-						<div id="search-overlay__results"></div>
-					  </div>
-
-				  </div>
-</div>
-
+			   	<div class="search-overlay no-display">
+				    <div class="search-overlay__top">
+				      	<div class="search-container-balk">
+					      	<i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+					      	<input class="search-term" placeholder="Waar ben je naar op zoek?" id="search-term" name="searchfield" type="text">
+				        	<span id="search-close"><i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i></span>
+				    	</div>
+					<div class="search-container">
+			  	<div id="search-overlay__results"></div>
+			</div>
+		</div>
+	</div>
 </div> <!-- page wrapper end -->
          <?php wp_footer(); ?>
          </body>
