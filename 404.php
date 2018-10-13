0c8587c21a496f69accc713cdef92fbd25eb09d5
Daniel van der Velden <danielvandervelden@Daniels-MacBook-Pro.local>
2018-10-13 13:31:31 +0200

    Fixed 404 on desktop as well

--- 404.php
+++ 404.php
@@ -1,10 +1,10 @@
 <?php get_header() ?>
 
     <div class="main-content-wrapper">
-        <div class="single-blog-post">
+        <div class="single-blog-post text-center">
             Daar ging iets niet helemaal goed! Je probeert een pagina te bekijken
             die niet (meer) bestaat. </br>
-            <a class="btn btn-pink spacing-top text-center" href="<?php echo get_home_url() ?>"><i class="fas fa fa-chevron-left"></i> Terug naar home!</a>
+            <a class="btn btn-pink btn-center spacing-top text-center" href="<?php echo get_home_url() ?>"><i class="fas fa fa-chevron-left"></i> Terug naar home!</a>
         </div>
     </div>
 
