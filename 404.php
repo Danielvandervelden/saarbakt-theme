606a5219bc70130b8bc80fcf24578f7d805e32d1
Daniel van der Velden <danielvandervelden@Daniels-MacBook-Pro.local>
2018-10-13 13:25:53 +0200

    And the actual 404 page...

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header() ?>
+
+    <div class="main-content-wrapper">
+        <div class="single-blog-post">
+            Daar ging iets niet helemaal goed! Je probeert een pagina te bekijken
+            die niet (meer) bestaat. </br>
+            <a class="btn btn-pink spacing-top text-center" href="<?php echo get_home_url() ?>"><i class="fas fa fa-chevron-left"></i> Terug naar home!</a>
+        </div>
+    </div>
+
+<?php get_footer() ?>
