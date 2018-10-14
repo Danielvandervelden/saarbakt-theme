fc72976361c069b10d5ac06f57aea533f0e27514
Daniel van der Velden <danielvandervelden@Daniels-MacBook-Pro.local>
2018-10-14 20:07:51 +0200

    Fixed the 404 page!

--- 404.php
+++ 404.php
@@ -2,9 +2,12 @@
 
     <div class="main-content-wrapper">
         <div class="single-blog-post text-center">
-            Daar ging iets niet helemaal goed! Je probeert een pagina te bekijken
-            die niet (meer) bestaat. </br>
-            <a class="btn btn-pink btn-center spacing-top text-center" href="<?php echo get_home_url() ?>"><i class="fas fa fa-chevron-left"></i> Terug naar home!</a>
+            <div class="spacing-bottom">
+                <img src="/wp-content/themes/saarbakt/images/saar404.png" alt="saar snap het ook niet meer" />
+            </div>
+            Ik snap ook niet waar die pagina naartoe is gegaan... Maar misschien is het recept niet verloren! 
+            Gebruik de zoek functie om het te vinden of </br>
+            <a class="btn btn-pink btn-center spacing-top text-center" href="<?php echo get_home_url() ?>"><i class="fas fa fa-chevron-left"></i> Ga terug naar home!</a>
         </div>
     </div>
 
